2ba8ab25e3cb59615ad6f75f7ae8846a3b36b8da
lam-gh <190052983+lam-gh@users.noreply.github.com>
2026-01-27 10:18:23 +0700

    not done yet

--- prac2/main.py
+++ prac2/main.py
@@ -1,7 +1,57 @@
 import pandas as pd
+import numpy as np
+import cv2
+from tensorflow import keras
+import os
+import segmentation_models as sm
 
 def main():
-    pass
+    # print(os.listdir("../data/hc18"))
+    # ['archive_hc18.zip', 'training_set', 'training_set_pixel_size_and_HC.csv', 'test_set', 'test_set_pixel_size.csv']
 
+    root_data_dir = "../data/hc18/"
+    train_dir = root_data_dir + "train_set"
+    test_dir = root_data_dir + "test_set"
+    train_csv = root_data_dir + "training_set_pixel_size_and_HC.csv"
+    test_csv = root_data_dir + "test_set_pixel_size.csv"
+
+    # Images: filename.png
+    # Annotated images: filename_Annotation.png
+    # shape: 800, 540
+
+    # print(train_df)
+    # print(test_df)
+
+    # print(x_train)
+    # print(y_train)
+    
+    # for filename, pixel_size in x_train:
+
+    model = sm.Unet(
+        'resnet34',
+        input_shape=(800, 540, 1),
+        classes=1,
+        activation="sigmoid"
+    )
+
+    model.compile(
+        optimizer="adam",
+        loss=sm.losses.bce_jaccard_loss,
+        metrics=[sm.metrics.iou_score]
+    )
+
+    # Load images
+    keras.utils.image_dataset_from_directory(
+        directory="../data/hc18/",
+
+    
+    )
+
+    #####
+    #
+    # NOT DONE YETTTTTTTT
+    #
+    #####
+         
 if __name__ == "__main__":
     main()
--- prac2/scratch.md
+++ prac2/scratch.md
@@ -1,1 +1,35 @@
+# The dataset
+
+- Bunch of images
+- training csv contains pixel size and circum
+- test csv contains only pixel
+- predicting circumference
+- measure the quality by uploading to website
+- what the hell is circum for -> that's the target
+
+Input: image
+output: pixel_size
+
+I will use a convolution neural network
+
+Regressional problem
+
+first method
+
+- Simple segmentation method
+- opencv to find longest
+- estimate ellipse
+
+
+second method
+
+- thresholding 
+- deep learning?
+
+I will go with the first method.
+
+- Keras U net
+- train using original ultrasound and annotated as truth
+- use this model to predict the mask for images in the test set
+- find the HC by calculate_hc(mask, pixel_size)
 
